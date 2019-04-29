f82a391428f68162a0e2da1cd6e2f14832573458
nycarts <jdalgety@gmail.com>
2019-04-28 19:19:05 -0500

    Added notes and process files to document process

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
-const rp = require('request-promise');
-const checksum = require('checksum'); 
+const rp = require('request-promise'); 
 const co = require('cheerio');
-
-// instantiate an empty variable outside the function so we can save its value
-let hash = "";
-
-function checkURL(siteToCheck) {
-  return rp(siteToCheck).then(HTMLresponse => {
-    
-    const $ = co.load(HTMLresponse);
-    let goalScorerString = "";
-
-    // use Cheerio to parse HTML response and find all search results
-    $(".statsTableContainer").each((i, element) => {
-      let topTwenty = element.children.map(a => a.name);
-      goalScorerString += `${topTwenty}`;
-    });
-    return checksum(goalScorerString);
-  })
-    .catch(err => {
-      console.log(`Could not complete fetch of ${url}: ${err}`);
-    });
-};
 
 const url = `https://www.premierleague.com/stats/top/players/goals`;
 
-// check every 10 seconds
-// doing this asynchronyously so our fetch is sure to resolve
-setInterval(async () => {
-  console.log(await checkURL(url));
-}, 10000);
+rp(url)
+  .then(response => console.log(response));
