12688cdb1a079254ce6479c408f44072b30a9c7e
nycarts <jdalgety@gmail.com>
2019-08-07 17:42:10 -0500

    updated packages and index.js to latest version

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ const co = require('cheerio');
 const url = `https://www.premierleague.com/stats/top/players/goals`;
 
 rp(url)
-  .then(response => console.log(response));
+  .then(res => {
+    res.setHeader("Content-Type", "text/javascript");
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  })
